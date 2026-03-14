ceb31aa23fce65ce3b634a1e95649466620b505e
chaihuasong <chaihuasong@gmail.com>
2026-03-15 00:18:58 +0800

    昵称优化

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SERVER="root@121.36.173.141"
+REMOTE_PATH="/root/htz-api-server-python"
+LOCAL_PATH="$(cd "$(dirname "$0")" && pwd)"
+
+echo "上传文件..."
+scp -r "$LOCAL_PATH/db.py" "$LOCAL_PATH/htz-api-server.py" "$LOCAL_PATH/request.py" "$LOCAL_PATH/static" "$SERVER:$REMOTE_PATH/"
+
+echo "重启服务..."
+ssh "$SERVER" "docker restart htz_server"
+
+echo "部署完成"
